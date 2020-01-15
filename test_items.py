f942f7847fdb446f6c91591f1c05f600ba658c9c
ilya.finagin <1millionbugs@gmail.com>
2020-01-15 20:31:20 +0300

    last change in test_items

--- test_items.py
+++ test_items.py
@@ -5,6 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_to_cart_presented(browser):
     browser.get(link)
     time.sleep(30)
-    button_add_to_cart = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    assert button_add_to_cart, \
-        f"Button add to cart is not presented"
+    button_add_to_cart = len(browser.find_elements(By.CSS_SELECTOR, "button[type='submit']"))
+    assert button_add_to_cart > 0, "Button add to cart is not presented"
